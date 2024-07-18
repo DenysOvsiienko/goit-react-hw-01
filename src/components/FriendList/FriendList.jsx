b71a8859acbf43b8e0d3aa66999d0615cf1a0477
Denys <denis.itsme.1@gmail.com>
2024-07-18 16:03:34 +0300

    add files

---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,23 @@
+import css from './FriendList.module.css';
+
+import FriendListItem from '../FriendListItem/FriendListItem';
+
+const FriendList = ({ friends }) => {
+  return (
+    <ul className={css.friendsList}>
+      {friends.map(friend => {
+        return (
+          <li key={friend.id}>
+            <FriendListItem
+              friendName={friend.name}
+              friendAvatarURL={friend.avatar}
+              friendStatus={friend.isOnline}
+            />
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
+export default FriendList;
---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,31 @@
+import css from './Profile.module.css';
+
+const Profile = ({ name, tag, location, image, stats }) => {
+  return (
+    <div className={css.profileCard}>
+      <div className={css.profileMainInfo}>
+        <img className={css.profileAvatar} src={image} alt={name} />
+        <p className={css.profileNameText}>{name}</p>
+        <p className={css.profileInfoText}>@{tag}</p>
+        <p className={css.profileInfoText}>{location}</p>
+      </div>
+
+      <ul className={css.profileStatsList}>
+        <li className={css.profileStatsListItem}>
+          <span>Followers</span>
+          <span className={css.profileStatsText}>{stats.followers}</span>
+        </li>
+        <li className={css.profileStatsListItem}>
+          <span>Views</span>
+          <span className={css.profileStatsText}>{stats.views}</span>
+        </li>
+        <li className={css.profileStatsListItem}>
+          <span>Likes</span>
+          <span className={css.profileStatsText}>{stats.likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Profile;
